8c82d7a462dda47ff54209cd7019e10656ea6e88
Nic Biddell <nicbiddell@pm.me>
2022-06-20 21:16:34 -0500

    Initial err handling

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/pkg/errors"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	if err := run(); err != nil {
+		fmt.Printf("Error during execution: %v\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	_ = flag.String("filename", "", "used to provide a file to convert")
+	flag.Parse()
+
+	areAllRequiredFlagsSet := false
+	flag.CommandLine.Visit(func(f *flag.Flag) {
+		if f.Name == "filename" {
+			areAllRequiredFlagsSet = true
+		}
+	})
+
+	if !areAllRequiredFlagsSet {
+		return errors.New("required parameters not passed in")
+	}
+
+	return nil
 }
