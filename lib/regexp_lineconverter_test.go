85b3faf32bdd5f47b8c663cca2c706682abbc274
Nic Biddell <nicbiddell@pm.me>
2022-06-21 19:44:27 -0500

    Make slightly more consistent with other test setups

--- lib/regexp_lineconverter_test.go
+++ lib/regexp_lineconverter_test.go
@@ -42,7 +42,7 @@ func TestTags(t *testing.T) {
 		},
 		{
 			name:           "A",
-			input:          `[myBlog](https://aws.nicbiddell.com/blog)`,
+			input:          "[myBlog](https://aws.nicbiddell.com/blog)",
 			expectedOutput: `<a href="https://aws.nicbiddell.com/blog">myBlog</a>`,
 		},
 	}
@@ -57,67 +57,5 @@ func TestTags(t *testing.T) {
 					testNum, test.name, test.expectedOutput, output)
 			}
 		})
-	}
-}
-
-func TestConversions(t *testing.T) {
-	tests := []struct {
-		name           string
-		input          string
-		expectedOutput string
-	}{
-		//		{
-		//			name: "First, simple sample",
-		//			input: `# Sample Document
-		//Hello!
-		//
-		//This is sample markdown for the [Mailchimp](https://www.mailchimp.com) homework assignment.`,
-		//			expectedOutput: `
-		//<h1>Sample Document</h1>
-		//
-		//<p>Hello</p>
-		//
-		//<p>This is sample markdown for the <a href="https://www.mailchimp.com">Mailchimp</a> homework assignment</p>`,
-		//		},
-		//		{
-		//			name: "Sample 2, nested, inline link",
-		//			input: `
-		//# Header one
-		//
-		//Hello there
-		//
-		//How are you?
-		//What's going on?
-		//
-		//## Another Header
-		//
-		//This is a paragraph [with an inline link](http://google.com). Neat, eh?
-		//
-		//## This is a header [with a link](http://yahoo.com)`,
-		//			expectedOutput: `
-		//<h1>Header one</h1>
-		//
-		//<p>Hello there</p>
-		//
-		//<p>How are you?
-		//What's going on?</p>
-		//
-		//<h2>Another Header</h2>
-		//
-		//<p>This is a paragraph <a href="http://google.com">with an inline link</a>. Neat, eh?</p>
-		//
-		//<h2>This is a header <a href="http://yahoo.com">with a link</a></h2>`,
-		//		},
-	}
-
-	for testNum, test := range tests {
-		converter := RegExpLineConverter{}
-
-		output := string(converter.ConvertLine([]byte(test.input)))
-		t.Log(output)
-		if output != test.expectedOutput {
-			t.Errorf("%d:%s - incorrect output. wanted=%s, got=%s",
-				testNum, test.name, test.expectedOutput, output)
-		}
 	}
 }
